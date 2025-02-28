fb0bb451f1dbcbed08b4e0f25dabba792c30dd71
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2025-02-28 16:55:42 -0700

    update user service

--- user-service/src/main/java/com/optimagrowth/license/UserServiceApplication.java
+++ user-service/src/main/java/com/optimagrowth/license/UserServiceApplication.java
@@ -12,10 +12,10 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @RefreshScope
-public class LicenseServiceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LicenseServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 	@Bean
