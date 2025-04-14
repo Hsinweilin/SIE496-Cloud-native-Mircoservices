30103e6d797865e4a005ef041cd861b7f94fedc3
Oliver <84431348+oliver-sey@users.noreply.github.com>
2025-04-13 20:49:01 -0700

    uncommenting setup.sh and adding review service

--- setup.sh
+++ setup.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
 # Build configserver service
-# echo "Building configserver service..."
-# cd configserver
-# mvn clean package
-# docker build --build-arg JAR_FILE=target/configserver-0.0.2-SNAPSHOT.jar -t ostock/configserver:0.0.2-SNAPSHOT .
-# cd ..
+echo "Building configserver service..."
+cd configserver
+mvn clean package
+docker build --build-arg JAR_FILE=target/configserver-0.0.2-SNAPSHOT.jar -t ostock/configserver:0.0.2-SNAPSHOT .
+cd ..
 
-# # Build user-service service
-# echo "Building user-service..."
-# cd user-service
-# mvn clean package
-# docker build --build-arg JAR_FILE=target/user-service-0.0.1-SNAPSHOT.jar -t ostock/user-service:0.0.1-SNAPSHOT .
-# cd ..
+# Build user-service service
+echo "Building user-service..."
+cd user-service
+mvn clean package
+docker build --build-arg JAR_FILE=target/user-service-0.0.1-SNAPSHOT.jar -t ostock/user-service:0.0.1-SNAPSHOT .
+cd ..
 
-# #Build inventory-order-service service
-# echo "Building inventory-order-service..."
-# cd inventory-order-service
-# mvn clean package
-# docker buildx build --build-arg JAR_FILE=target/inventory-order-service-0.0.1-SNAPSHOT.jar -t ostock/inventory-order-service:0.0.1-SNAPSHOT .
-# cd ..
+#Build inventory-order-service service
+echo "Building inventory-order-service..."
+cd inventory-order-service
+mvn clean package
+docker buildx build --build-arg JAR_FILE=target/inventory-order-service-0.0.1-SNAPSHOT.jar -t ostock/inventory-order-service:0.0.1-SNAPSHOT .
+cd ..
 
-# #build payment-service service
-# echo "Building payment-service..."
-# cd payment-service
-# mvn clean package
-# docker build --build-arg JAR_FILE=target/payment-service-0.0.1-SNAPSHOT.jar -t ostock/payment-service:0.0.1-SNAPSHOT .
-# cd ..
+#build payment-service service
+echo "Building payment-service..."
+cd payment-service
+mvn clean package
+docker build --build-arg JAR_FILE=target/payment-service-0.0.1-SNAPSHOT.jar -t ostock/payment-service:0.0.1-SNAPSHOT .
+cd ..
 
-# echo "Building eureka..."
-# cd eurekaserver
-# mvn clean package
-# docker buildx build --build-arg JAR_FILE=target/eurekaserver-0.0.2-SNAPSHOT.jar -t ostock/eurekaserver:0.0.2-SNAPSHOT .
-# cd ..
+#build review-service service
+echo "Building review-service..."
+cd review-service
+mvn clean package
+docker build --build-arg JAR_FILE=target/review-service-0.0.1-SNAPSHOT.jar -t ostock/review-service:0.0.1-SNAPSHOT .
+cd ..
+
+# build eureka
+echo "Building eureka..."
+cd eurekaserver
+mvn clean package
+docker buildx build --build-arg JAR_FILE=target/eurekaserver-0.0.2-SNAPSHOT.jar -t ostock/eurekaserver:0.0.2-SNAPSHOT .
+cd ..
 
 
 echo "Building gatewayserver..."
